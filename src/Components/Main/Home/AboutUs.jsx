f3654813e678f32f18a941ecad111bd5bf77f766
abusaidshabib <abusaidshabib712@.gmail.com>
2023-05-26 23:10:52 +0600

    hero crop

--- src/Components/Main/Home/AboutUs.jsx
+++ src/Components/Main/Home/AboutUs.jsx
@@ -1,14 +1,31 @@
 const AboutUs = () => {
   return (
     <div className="min-h-screen bg-background-500 grid items-center px-20">
-      <p className="text-5xl text-primary-500 font-serif">
-        Empowering Minds,
-        <span className="text-p-white">
-          {" "}
-          Inspiring{" "}
-          <span className="underline-offset-2 border-b-4 border-secondary-500">Futures</span>
-        </span>
-      </p>
+      <div>
+        <p className="text-5xl text-primary-500 font-serif leading-relaxed">
+          Empowering Minds,
+          <span className="text-p-white">
+            {" "}
+            Inspiring{" "}
+            <span className="underline-offset-2 border-b-4 border-secondary-500">
+              Futures
+            </span>
+          </span>
+        </p>
+        <p className="py-6"></p>
+        <p className="text-p-white font-sans text-lg">
+          Welcome to Univast University, where knowledge meets opportunity and
+          dreams take flight. As a renowned institution of higher education, we
+          are dedicated to empowering minds and inspiring futures. Our
+          commitment to academic excellence, innovative teaching methodologies,
+          and a vibrant campus community sets us apart.{" "}
+        </p>
+      </div>
+      <div className="grid grid-cols-4">
+        <div className="">
+          
+        </div>
+      </div>
     </div>
   );
 };
