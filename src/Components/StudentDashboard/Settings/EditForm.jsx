f5fe0b8a03649900a0ef1e6ca62d04dc76c6d9be
abusaidshabib <abusaidshabib712@.gmail.com>
2023-05-28 20:03:14 +0600

    settings done

---
+++ src/Components/StudentDashboard/Settings/EditForm.jsx
@@ -0,0 +1,188 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiFillEdit } from "react-icons/ai";
+
+const EditForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  const [status, setStatus] = useState(true);
+  return (
+    <form
+      className="bg-white p-10 grid grid-cols-3 gap-10 rounded-lg"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex justify-between items-center text-gray-900 col-span-3">
+        <div>
+          <p className="text-4xl font-semibold">Apply Information</p>
+        </div>
+        <div>
+          <AiFillEdit
+            onClick={() => setStatus(!status)}
+            className={`text-2xl ${!status ? "text-blue-600" : ""} `}
+          />
+        </div>
+      </div>
+      <p className="col-span-3 text-lg"> If you forgot your password then click <span className="text-blue-600 underline font-bold">Forgot password</span> and check your gmail
+      </p>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="firstname">
+          First Name
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="Golam"
+          {...register("firstname", { required: true })}
+        />
+      </div>
+
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="lastname">
+          Last Name
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="Mehedi"
+          {...register("lastname", { required: true })}
+        />
+      </div>
+
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="marital">
+          Marital Status
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="married"
+          {...register("marital", { required: true })}
+        />
+      </div>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="email">
+          Email
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="moiragesi@gmail.com"
+          {...register("email", { required: true })}
+        />
+      </div>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="mobile">
+          Mobile Number
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="0123456789123"
+          {...register("mobile", { required: true })}
+        />
+      </div>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="profile-image">
+          Profile Image
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="file"
+          accept="image/*"
+          disabled={status}
+          {...register("profile-image", { required: true })}
+        />
+      </div>
+      <p className="col-span-3 text-2xl underline">Present Address</p>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="state-division">
+          Division
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="Panir tanki"
+          {...register("state-division", { required: true })}
+        />
+      </div>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="thana">
+          Thana
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="Panir tanki"
+          {...register("thana", { required: true })}
+        />
+      </div>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="city">
+          city
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="Biriyani house"
+          {...register("city", { required: true })}
+        />
+      </div>
+      <div className="grid grid-cols-3 items-center border-b">
+        <label className="text-lg col-span-1" htmlFor="zip-code">
+          Zip Code
+        </label>
+        <input
+          className={`w-full outline-none py-2 text-lg font-normal col-span-2 bg-transparent ${
+            status ? "" : "border-b-2 border-blue-600"
+          }`}
+          type="text"
+          disabled={status}
+          defaultValue="Biriyani house"
+          {...register("zip-code", { required: true })}
+        />
+      </div>
+      {!status ? (
+        <input
+          className="py-3 bg-gray-900 rounded-md text-white uppercase tracking-widest text-lg"
+          type="submit"
+        />
+      ) : (
+        <></>
+      )}
+    </form>
+  );
+};
+
+export default EditForm;
--- src/Pages/StudentDashboard/Ssetting.jsx
+++ src/Pages/StudentDashboard/Ssetting.jsx
@@ -1,67 +1,18 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { AiFillEdit } from "react-icons/ai";
+import EditForm from "../../Components/StudentDashboard/Settings/EditForm";
+
 
 const Ssetting = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
 
-  const [status, setStatus] = useState(true);
  
 
   return (
-    <div className="bg-gray-200 w-full min-h-screen p-5 font-semibold">
-      <div className="flex justify-between items-center text-gray-900">
-        <div>
-          <p className="text-4xl pb-5 font-semibold">Apply Information</p>
+    <div className="bg-gray-200 w-full min-h-[calc(100vh-80px)] font-semibold p-10 grid gap-10">
+      <EditForm></EditForm>
+        <div className="bg-white rounded-md grid place-content-center">
+          <p className="text-red-600 text-xl">
+            If you want to edit more details info. Please contact with admin. Because those are very sensitive details for edit every-time.
+          </p>
         </div>
-        <div>
-          <AiFillEdit
-            onClick={() => setStatus(!status)}
-            className={`text-2xl ${!status ? "text-blue-600" : ""} `}
-          />
-        </div>
-      </div>
-      <form
-        className="bg-white p-10 grid gap-5"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="grid grid-cols-3 items-center border-b">
-          <label className="text-2xl col-span-1" htmlFor="firstname">
-            Name
-          </label>
-          <input
-            className={`w-full outline-none py-5 text-xl font-normal rounded-md col-span-2 ${
-              status ? "bg-transparent" : "bg-gray-200 px-5"
-            }`}
-            type="text"
-            disabled={status}
-            defaultValue="Golam"
-            {...register("firstname", { required: true })}
-          />
-        </div>
-        <div>
-          <label className="text-2xl" htmlFor="firstname">
-            Last Name
-          </label>
-          <input
-            className={`w-full outline-none py-5 text-xl font-normal rounded-md ${
-              status ? "bg-transparent" : "bg-gray-200 px-5"
-            }`}
-            type="text"
-            disabled={status}
-            defaultValue="Mehedi"
-            {...register("firstname", { required: true })}
-          />
-          <br />
-        </div>
-        <input type="submit" />
-      </form>
     </div>
   );
 };
