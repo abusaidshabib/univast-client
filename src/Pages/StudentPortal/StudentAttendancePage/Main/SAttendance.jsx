212ed1fbdd2c4bee6ba1e86f779529fc408a461c
pulok-thedeveloper <mdpulokhasan@gmail.com>
2023-05-31 23:39:38 +0600

    create attendance page

--- src/Pages/StudentPortal/StudentAttendancePage/Main/SAttendance.jsx
+++ src/Pages/StudentPortal/StudentAttendancePage/Main/SAttendance.jsx
@@ -1,13 +1,9 @@
-import SFinantialInfo from "../../StudentPaymentPage/Sections/SFinancialInfo/SFinantialInfo";
-import SPaymentForm from "../../StudentPaymentPage/Sections/SPaymentForm/SPaymentForm";
-import SPaymentSlip from "../../StudentPaymentPage/Sections/SPaymentSlip/SPaymentSlip";
+import SMainAttendance from "../Sections/MainAttendanceTable/SMainAttendance";
 
 const SAttendance = () => {
   return (
     <div className="min-h-[calc(100vh-80px)] w-full bg-gray-200 p-5 grid gap-5">
-      <SFinantialInfo></SFinantialInfo>
-      <SPaymentForm></SPaymentForm>
-      <SPaymentSlip></SPaymentSlip>
+      <SMainAttendance></SMainAttendance>
     </div>
   );
 };
