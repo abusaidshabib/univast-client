ea698fea45c743ea15a67a9f6441d9e60041e147
abusaidshabib <abusaidshabib712@gmail.com>
2023-09-03 19:46:11 +0600

    new menu

--- src/Pages/MainWebsite/Shared/Navbar.jsx
+++ src/Pages/MainWebsite/Shared/Navbar.jsx
@@ -89,31 +89,7 @@ const Navbar = () => {
                 About
               </li>
               <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/control-panel">Control Panel</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/student">Student Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/teacher">Teacher Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/enroll">Enroll Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/admin">Admin Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/course">Course Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/exam">Exam Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/financial">Financial Portal</Link>
-              </li>
-              <li className="cursor-pointer hover:underline hover:text-primary-white underline-offset-[12px] transition">
-                <Link to="/resource">Resource Portal</Link>
+                <Link to="/control-panel">C-Panel</Link>
               </li>
             </ul>
           </div>
