c487511a959a9e5d75e931e07d7896d89ccbc784
abusaidshabib <abusaidshabib712@gmail.com>
2023-09-03 20:04:51 +0600

    control panel

--- src/Pages/ControlPanel/Main/ControlPanel.jsx
+++ src/Pages/ControlPanel/Main/ControlPanel.jsx
@@ -8,75 +8,86 @@ import { BsCashCoin, BsQuestionDiamond } from "react-icons/bs";
 
 const ControlPanel = () => {
   return (
-    <div className="grid grid-cols-12 gap-5 p-24 bg-gray-200 min-h-screen text-gray-900 font-sans">
-      <Link className="w-full" to="/student">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <SiHtmlacademy />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">Student</p>
-      </Link>
-      <Link className="w-full" to="/teacher">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <FaChalkboardTeacher />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">Teacher</p>
-      </Link>
-      <Link className="w-full" to="/enroll">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <MdOutlineAddTask />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">Enroll</p>
-      </Link>
-      <Link className="w-full" to="/admin">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <RiAdminLine />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">Admin</p>
-      </Link>
-      <Link className="w-full" to="/course">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <BiBookAlt />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">Courses</p>
-      </Link>
-      <Link className="w-full" to="/exam">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <BsQuestionDiamond />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">Exam</p>
-      </Link>
-      <Link className="w-full" to="/financial">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <BsCashCoin />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">
-          Financial
-        </p>
-      </Link>
-      <Link className="w-full" to="/resource">
-        <p className="text-4xl grid place-content-center text-p-white">
-          <span className="bg-primary-blue p-3 rounded-full">
-            <BiCollection />
-          </span>
-        </p>
-        <p className="text-lg grid place-content-center font-medium">
-          Resource
-        </p>
-      </Link>
+    <div className="bg-gray-200 p-24">
+      <p className="text-4xl font-semibold text-center pb-14">Applications</p>
+      <div className="grid grid-cols-4 min-h-screen text-gray-900 font-sans max-w-2xl mx-auto">
+        <Link className="w-full" to="/student">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <SiHtmlacademy />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">
+            Student
+          </p>
+        </Link>
+        <Link className="w-full" to="/teacher">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <FaChalkboardTeacher />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">
+            Teacher
+          </p>
+        </Link>
+        <Link className="w-full" to="/enroll">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <MdOutlineAddTask />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">
+            Enroll
+          </p>
+        </Link>
+        <Link className="w-full" to="/admin">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <RiAdminLine />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">Admin</p>
+        </Link>
+        <Link className="w-full" to="/course">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <BiBookAlt />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">
+            Courses
+          </p>
+        </Link>
+        <Link className="w-full" to="/exam">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <BsQuestionDiamond />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">Exam</p>
+        </Link>
+        <Link className="w-full" to="/financial">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <BsCashCoin />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">
+            Financial
+          </p>
+        </Link>
+        <Link className="w-full" to="/resource">
+          <p className="text-5xl grid place-content-center text-p-white">
+            <span className="bg-secondary-blue hover:bg-primary-blue p-3 rounded-full">
+              <BiCollection />
+            </span>
+          </p>
+          <p className="text-xl grid place-content-center font-medium">
+            Resource
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
