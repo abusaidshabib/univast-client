fdd3d1495fc9238cfede7479a48e67d02d56011c
pulok-thedeveloper <mdpulokhasan@gmail.com>
2023-09-06 20:41:48 +0600

    set user

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -7,29 +7,44 @@ import {
 } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
-import { useGetUserByFirebaseIdQuery } from "../features/user/userApi";
+// import { useGetUserByFirebaseIdQuery } from "../features/user/userApi";
 
 export const AuthContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 const UserContext = ({ children }) => {
-  const {data} = useGetUserByFirebaseIdQuery()
   // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [oldUser, setOldUser] = useState(null);
+  console.log(oldUser)
   const [user, setUser] = useState(null);
-  console.log(user)
 
+  // const {data: userData} = useGetUserByFirebaseIdQuery(user?.uid)
+  // console.log(userData);
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [loading, setLoading] = useState(true);
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
+      setOldUser(currentUser);
       setLoading(false);
     });
+    if (oldUser?.uid) {
+      console.log(oldUser?.uid);
+      const firebaseId = oldUser?.uid;
+  
+      fetch(`http://localhost:8000/api/v1/users?firebaseId=${firebaseId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.data.user.role);
+          setUser(data.data.user)
+        });
+    }
     return () => unSubscribe();
-  }, [user]);
+  }, [oldUser]);
 
+
+  console.log(user);
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
