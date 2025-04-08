9f8757fbf429b9c49161ed03dee79c0dac84d183
pulok-thedeveloper <mdpulokhasan@gmail.com>
2025-04-08 12:43:36 +0600

    tution fees page fix

--- src/Pages/MainWebsite/TuitionFees/Main/TuitionFees.jsx
+++ src/Pages/MainWebsite/TuitionFees/Main/TuitionFees.jsx
@@ -3,75 +3,81 @@ const TuitionFees = () => {
     <div className="p-20">
       <p className="text-6xl font-extrabold leading-relaxed pb-10">
         {" "}
-        Tuition Fees of every department
+        Tuition Fees
       </p>
-      <p className="font-semibold text-2xl">Fees of CSE</p>
+      <p className="font-semibold text-2xl mb-3">CSE (138 Credit in 12 semester)</p>
       <table className="border-collapse border border-slate-400 w-full text-lg">
         <thead>
           <tr>
-            <th className="border border-gray-400 p-2 ">Code</th>
-            <th className="border border-gray-400 p-2 ">Title</th>
-            <th className="border border-gray-400 p-2 ">Credit</th>
-            <th className="border border-gray-400 p-2 ">Teacher</th>
-            <th className="border border-gray-400 p-2 ">Grade</th>
+            <th className="border border-gray-400 p-2 ">Waiver %</th>
+            <th className="border border-gray-400 p-2 ">Course Fee</th>
+            <th className="border border-gray-400 p-2 ">Additional 10%</th>
+            <th className="border border-gray-400 p-2 ">Per Semester</th>
+            <th className="border border-gray-400 p-2 ">
+              Admission fee + 1st installment
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-gray-400 p-2 ">CSE-4201</td>
-            <td className="border border-gray-400 p-2 ">English</td>
-            <td className="border border-gray-400 p-2 ">50</td>
-            <td className="border border-gray-400 p-2">Ketu</td>
-            <td className="border border-gray-400 p-2">Boroloker A+</td>
+            <td className="border border-gray-400 p-2 text-center ">20%</td>
+            <td className="border border-gray-400 p-2 text-center ">469600</td>
+            <td className="border border-gray-400 p-2 text-center ">422640</td>
+            <td className="border border-gray-400 p-2 text-center">35220</td>
+            <td className="border border-gray-400 p-2 text-center">25740</td>
           </tr>
           <tr>
-            <td className="border border-gray-400 p-2 ">CSE-4201</td>
-            <td className="border border-gray-400 p-2 ">English</td>
-            <td className="border border-gray-400 p-2 ">50</td>
-            <td className="border border-gray-400 p-2">kito</td>
-            <td className="border border-gray-400 p-2">Goriber A+</td>
+            <td className="border border-gray-400 p-2 text-center ">30%</td>
+            <td className="border border-gray-400 p-2 text-center ">420900</td>
+            <td className="border border-gray-400 p-2 text-center ">378810</td>
+            <td className="border border-gray-400 p-2 text-center">31568</td>
+            <td className="border border-gray-400 p-2 text-center">24523</td>
           </tr>
           <tr>
-            <td className="border border-gray-400 p-2 ">CSE-4201</td>
-            <td className="border border-gray-400 p-2 ">English</td>
-            <td className="border border-gray-400 p-2 ">50</td>
-            <td className="border border-gray-400 p-2">kandu</td>
-            <td className="border border-gray-400 p-2">Moddhobitter A+</td>
+            <td className="border border-gray-400 p-2 text-center ">50%</td>
+            <td className="border border-gray-400 p-2 text-center ">323500</td>
+            <td className="border border-gray-400 p-2 text-center ">291150</td>
+            <td className="border border-gray-400 p-2 text-center">24263</td>
+            <td className="border border-gray-400 p-2 text-center">22088</td>
           </tr>
         </tbody>
       </table>
-      <p className="font-semibold text-2xl pt-10">Fees of CSE</p>
+      <p className="font-semibold text-2xl mb-3 pt-10">
+        BBA (135 Credit in 12 semester)
+      </p>
       <table className="border-collapse border border-slate-400 w-full text-lg">
         <thead>
           <tr>
-            <th className="border border-gray-400 p-2 ">Code</th>
-            <th className="border border-gray-400 p-2 ">Title</th>
-            <th className="border border-gray-400 p-2 ">Credit</th>
-            <th className="border border-gray-400 p-2 ">Teacher</th>
-            <th className="border border-gray-400 p-2 ">Grade</th>
+            <th className="border border-gray-400 p-2 ">Waiver %</th>
+            <th className="border border-gray-400 p-2 ">Course Fee</th>
+            <th className="border border-gray-400 p-2 ">Additional 10%</th>
+            <th className="border border-gray-400 p-2 ">Per Semester</th>
+            <th className="border border-gray-400 p-2 ">
+              Admission fee + 1st installment
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-gray-400 p-2 ">CSE-4201</td>
-            <td className="border border-gray-400 p-2 ">English</td>
-            <td className="border border-gray-400 p-2 ">50</td>
-            <td className="border border-gray-400 p-2">Ketu</td>
-            <td className="border border-gray-400 p-2">Boroloker A+</td>
+            <td className="border border-gray-400 p-2 text-center ">20%</td>
+            <td className="border border-gray-400 p-2 text-center ">469600</td>
+            <td className="border border-gray-400 p-2 text-center ">422640</td>
+            <td className="border border-gray-400 p-2 text-center">35220</td>
+            <td className="border border-gray-400 p-2 text-center">25740</td>
           </tr>
           <tr>
-            <td className="border border-gray-400 p-2 ">CSE-4201</td>
-            <td className="border border-gray-400 p-2 ">English</td>
-            <td className="border border-gray-400 p-2 ">50</td>
-            <td className="border border-gray-400 p-2">kito</td>
-            <td className="border border-gray-400 p-2">Goriber A+</td>
+            <td className="border border-gray-400 p-2 text-center ">30%</td>
+            <td className="border border-gray-400 p-2 text-center ">420900</td>
+            <td className="border border-gray-400 p-2 text-center ">378810</td>
+            <td className="border border-gray-400 p-2 text-center">31568</td>
+            <td className="border border-gray-400 p-2 text-center">24523</td>
           </tr>
           <tr>
-            <td className="border border-gray-400 p-2 ">CSE-4201</td>
-            <td className="border border-gray-400 p-2 ">English</td>
-            <td className="border border-gray-400 p-2 ">50</td>
-            <td className="border border-gray-400 p-2">kandu</td>
-            <td className="border border-gray-400 p-2">Moddhobitter A+</td>
+            <td className="border border-gray-400 p-2 text-center ">50%</td>
+            <td className="border border-gray-400 p-2 text-center ">323500</td>
+            <td className="border border-gray-400 p-2 text-center ">291150</td>
+            <td className="border border-gray-400 p-2 text-center">24263</td>
+            <td className="border border-gray-400 p-2 text-center">22088</td>
           </tr>
         </tbody>
       </table>
